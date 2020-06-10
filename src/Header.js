47ff13fb19c1281beab26af9f492953f3a72c414
Marina <marinafaenze@hotmail.com>
2020-06-10 16:52:06 -0300

    align items in Header Component

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ function Header() {
             <img 
                 src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="Problem?" 
             />
-            <p>Meme Generator</p>
+            <h1>Meme Generator</h1>
         </header>
     );
 }
