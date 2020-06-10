df6c64d236e8529c133a7633664ce780885f9df2
Marina <marinafaenze@hotmail.com>
2020-06-10 16:39:53 -0300

    Header and MemeGenerator imported to App Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import MemeGenerator from './MemeGenerator';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
   );
 }
 
